6c529dc6025d7082b986978257d83968255b2d78
Zeifen <60271815+Zeifen@users.noreply.github.com>
2025-05-08 15:44:38 -0600

    Feat: Front parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name, setName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
+  const [editId, setEditId] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    
+  }, []);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Prueba");
+  }
+
+  const handleEdit = (user) => {
+    setName(user.name);
+    setFirstName(user.firstName);
+    setSecondName(user.secondName);
+    setEditId(user._id);
+  }
+
+  const handleEliminate = () => {
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <h2 className='mb-2'>Ejercicio</h2>
+          <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Nombre' className='form-control mb-2' value={name} onChange={(e) => setName(e.target.value)} required></input>
+            <input type='text' placeholder='Apellido Paterno' className='form-control mb-2' value={firstName} onChange={(e) => setFirstName(e.target.value)} required></input>
+            <input type='text' placeholder='Apellido Materno' className='form-control mb-2' value={secondName} onChange={(e) => setSecondName(e.target.value)} required></input>
+            <button className='btn btn-info'>
+              {editId? 'Actualizar' : 'Enviar'}
+            </button>
+          </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='container'>
+        <h3 className='mt-5 mb-5'>Usuarios registrados</h3>
+        {users.length === 0 ? <p>No hay us. registrado</p> : 
+          <ul>
+            {users.map(user => (
+              <li className='list-group-item' key={user._id}>
+                <span>{user.name} {user.firstName} {user.secondName}</span>
+                <span>
+                  <button className='btn btn-warning me-2' onClick={() => handleEdit(user)}>Editar</button>
+                  <button className='btn btn-danger' onClick={() => handleEliminate(user._id)}>Eliminar</button>
+                </span>
+              </li>
+            ))}
+          </ul>
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
